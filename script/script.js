b699d2d624dd6b76998bc91ea338ea436dbd618a
Sohelrana2815 <sohel152302@gmail.com>
2024-02-21 13:12:11 +0600

    Alpha clash hide and show function work

--- script/script.js
+++ script/script.js
@@ -2,11 +2,15 @@
 //   // 1. Hide the home-screen section
 //   const homeSection = document.getElementById("home-screen");
 //   homeSection.classList.add("hidden");
-//   // 2. Show the playgroun section
+//   // 2. Show the playground section
 //   const playGround = document.getElementById("play-ground");
 //   console.log(playGround.classList);
 //   playGround.classList.remove("hidden");
 // }
+
+function continueGame() {
+    //1. Generate a random alphabet
+}
 
 function playNow() {
   hideElementById("home-screen");
--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,9 @@ function showElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
+
+function getARandomAlphabet() {
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
+  console.log(alphabets);
+}
