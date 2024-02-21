0aafba6dc11176c2b00747a507c978e90af56fb7
Sohelrana2815 <sohel152302@gmail.com>
2024-02-21 22:01:47 +0600

    Alpha clash hide and show function work

--- script/script.js
+++ script/script.js
@@ -8,11 +8,39 @@
 //   playGround.classList.remove("hidden");
 // }
 
+function keyboardKeyUpEvent(event) {
+  const playerPressed = event.key;
+  console.log("player pressed", playerPressed);
+  // Expected key to press
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  console.log(playerPressed, expectedAlphabet);
+
+  // Checked matched or not
+
+  if (playerPressed === expectedAlphabet) {
+    console.log("You get a point");
+  } else {
+    console.log("You lose a life");
+  }
+}
+
+document.addEventListener("keyup", keyboardKeyUpEvent);
+
 function continueGame() {
-    //1. Generate a random alphabet
+  //1. Generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  console.log("Your Random Alphabet ", alphabet);
+  //2. Show the random Alphabet to the screen
+
+  const currentAlphabet = document.getElementById("current-alphabet");
+  currentAlphabet.innerText = alphabet;
+  setBackgroundColorById(alphabet);
 }
 
 function playNow() {
   hideElementById("home-screen");
   showElementById("play-ground");
+  continueGame();
 }
--- script/utility.js
+++ script/utility.js
@@ -2,14 +2,36 @@ function hideElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add("hidden");
 }
-
 function showElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
 
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-[#FFA500]");
+}
+
+// function getARandomAlphabet() {
+//   //get or create an alphabet array
+//   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+//   const alphabets = alphabetString.split("");
+//   // console.log(alphabets);
+
+//   //get a random index between 0-25
+//   const randomNumber = Math.random() * 25;
+//   const index = Math.round(randomNumber);
+//   // console.log(index);
+//   const alphabet = alphabets[index];
+//   // console.log(index, alphabet);
+//   return alphabet;
+// }
+
 function getARandomAlphabet() {
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
   const alphabets = alphabetString.split("");
-  console.log(alphabets);
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+  const alphabet = alphabets[index];
+  return alphabet;
 }
