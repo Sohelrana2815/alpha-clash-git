e8d183312306651e258e1e16ebcd5948dc29a818
Sohelrana2815 <sohel152302@gmail.com>
2024-02-22 07:23:25 +0600

    Added the Score and life logic

--- script/script.js
+++ script/script.js
@@ -10,28 +10,79 @@
 
 function keyboardKeyUpEvent(event) {
   const playerPressed = event.key;
-  console.log("player pressed", playerPressed);
-  // Expected key to press
+  console.log(playerPressed);
+
   const currentAlphabetElement = document.getElementById("current-alphabet");
   const currentAlphabet = currentAlphabetElement.innerText;
   const expectedAlphabet = currentAlphabet.toLowerCase();
   console.log(playerPressed, expectedAlphabet);
 
-  // Checked matched or not
-
   if (playerPressed === expectedAlphabet) {
     console.log("You get a point");
-  } else {
-    console.log("You lose a life");
+
+    // Update score:
+    // 1. Get the current score
+
+    const currentScoreElement = document.getElementById("current-score");
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+
+    // 2. Increase score by 1 point if you pressed the correct alphabet
+
+    const nweScore = currentScore + 1;
+
+    // 3. Show the updated goal
+
+    currentScoreElement.innerText = nweScore;
+    // Set new round
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+  } 
+  
+  else {
+    console.log("You lose a point");
+
+    // 1. Get the current life number
+
+    const currentLifeElement = document.getElementById("current-life");
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
+    console.log(currentLife);
+
+    // 2. Lose a life if you pressed a wrong key
+    const newLife = currentLife - 1;
+
+    // 3. Display the updated life count
+
+    currentLifeElement.innerText = newLife;
   }
 }
+
+// function keyboardKeyUpEvent(event) {
+//   const playerPressed = event.key;
+//   console.log("player pressed", playerPressed);
+//   // Expected key to press
+//   const currentAlphabetElement = document.getElementById("current-alphabet");
+//   const currentAlphabet = currentAlphabetElement.innerText;
+//   const expectedAlphabet = currentAlphabet.toLowerCase();
+//   console.log(playerPressed, expectedAlphabet);
+
+//   // Checked matched or not
+
+//   if (playerPressed === expectedAlphabet) {
+//     console.log("You get a point");
+//   } else {
+//     console.log("You lose a life");
+//   }
+// }
 
 document.addEventListener("keyup", keyboardKeyUpEvent);
 
 function continueGame() {
   //1. Generate a random alphabet
   const alphabet = getARandomAlphabet();
-  console.log("Your Random Alphabet ", alphabet);
+  // console.log("Your Random Alphabet ", alphabet);
   //2. Show the random Alphabet to the screen
 
   const currentAlphabet = document.getElementById("current-alphabet");
--- script/utility.js
+++ script/utility.js
@@ -12,6 +12,11 @@ function setBackgroundColorById(elementId) {
   element.classList.add("bg-[#FFA500]");
 }
 
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-[#FFA500]");
+}
+
 // function getARandomAlphabet() {
 //   //get or create an alphabet array
 //   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
